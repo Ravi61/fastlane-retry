2fa1563b273bc8600b45500c7849e5728e62813e
Ravi Aggarwal <ravi.aggarwal61@gmail.com>
2020-06-02 16:26:48 +0530

    initial commit

--- DemoUITest/ViewController.swift
+++ DemoUITest/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import os.signpost
 
 class ViewController: UIViewController {
+    
+    private let log = OSLog(subsystem: "com.demo.uitest", category: "uitest.category")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        os_signpost(.begin, log: self.log, name: "load")
+        sleep(2)
+        os_signpost(.end, log: self.log, name: "load")
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        os_signpost(.begin, log: self.log, name: "layout_subviews")
+        sleep(2)
+        os_signpost(.end, log: self.log, name: "layout_subviews")
+    }
 }
 
